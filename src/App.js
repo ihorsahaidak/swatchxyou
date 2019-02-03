6bf810300f89bc6ad74c1e9c5301f8f906b7c861
ihsah <ihorsahaydak@gmail.com>
2019-02-03 17:12:16 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component, useState } from 'react'
-import { Switch, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React, {Component} from 'react'
+import {Switch, Route} from 'react-router-dom'
+import {connect} from 'react-redux'
 import withLayout from './Layout'
 import Home from 'pages/Home'
 import StartBuilding from 'pages/StartBuilding'
@@ -12,13 +12,7 @@ import Loop from 'pages/Choosers/Loop'
 import Accessories from 'pages/Choosers/Accessories'
 import Preview from 'pages/Preview'
 import About from 'pages/About'
-
-
-import {
-    CSSTransition,
-    TransitionGroup
-} from 'react-transition-group';
-
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 @withLayout
 class App extends Component {
@@ -48,8 +42,6 @@ class App extends Component {
   render() {
         return (
             <Switch>
-
-
                 <Route
                     render={({ location }) => {
                         const { pathname } = location;
@@ -66,18 +58,18 @@ class App extends Component {
                                     <Route
                                         location={location}
                                         render={() => (
-                                            <Switch>
-                                                <Route exact path='/' component={Home} />
-                                                <Route exact path='/start-building' component={StartBuilding} />
-                                                <Route exact path='/watch-model' component={WatchModel} />
-                                                <Route exact path='/watch-head' component={WatchHead} />
-                                                <Route exact path='/upper-strap' component={UpperStrap} />
-                                                <Route exact path='/lower-strap' component={LowerStrap} />
-                                                <Route exact path='/loop' component={Loop} />
-                                                <Route exact path='/accessories' component={Accessories} />
-                                                <Route exact path='/preview' component={Preview} />
-                                                <Route path='/about' component={About} />
-                                            </Switch>
+                                        <Switch>
+                                            <Route exact path='/' component={Home} />
+                                            <Route exact path='/start-building' component={StartBuilding} />
+                                            <Route exact path='/watch-model' component={WatchModel} />
+                                            <Route exact path='/watch-head' component={WatchHead} />
+                                            <Route exact path='/upper-strap' component={UpperStrap} />
+                                            <Route exact path='/lower-strap' component={LowerStrap} />
+                                            <Route exact path='/loop' component={Loop} />
+                                            <Route exact path='/accessories' component={Accessories} />
+                                            <Route exact path='/preview' component={Preview} />
+                                            <Route path='/about' component={About} />
+                                        </Switch>
                                         )}
                                     />
                                 </CSSTransition>
@@ -94,5 +86,5 @@ export default connect(
     state => ({}),
     dispatch => ({}),
     null,
-    { pure: false }
-)(App);
+    {pure: false}
+) (App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import reducer from './reducers'
 import App from './App'
 
